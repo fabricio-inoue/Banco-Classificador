34bbc75fa3daebca320776e27b65638bdc97068d
Daniel damasceno <130251767+Yoshikatsuara@users.noreply.github.com>
2023-10-01 19:17:51 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -143,7 +143,7 @@ elif selected_page == "Modelo de Random Fortest Treinado":
 
 elif selected_page == "Teste":
 
-col1, col2 = st.columns(2)
->>> col1.write(st.image('"D:\_Daniel\Downloads\saiba-como-fazer-uma-estrategia-de-negocio-de-acordo-com-o-perfil-da-empresa-810x540.jpeg"'))
->>> col2.write(st.image('negocio-para-abrir-em-2023.png'))
+    col1, col2 = st.columns(2)
+        col1.write(st.image('"D:\_Daniel\Downloads\saiba-como-fazer-uma-estrategia-de-negocio-de-acordo-com-o-perfil-da-empresa-810x540.jpeg"'))
+        col2.write(st.image('negocio-para-abrir-em-2023.png'))
 
