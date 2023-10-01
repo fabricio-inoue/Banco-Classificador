21515c547f48e13d2b561339a7bb878d5e99383e
fabricio-inoue <103461034+fabricio-inoue@users.noreply.github.com>
2023-10-01 17:27:48 -0300

    First Commit

---
+++ Dashboard.py
@@ -0,0 +1,146 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import numpy as np
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_title='Dashboard_Bank', layout='centered')
+
+df = pd.read_csv('bank-additional-full.csv', sep=';')
+
+selected_page = st.sidebar.selectbox("Selecione a página", ["Dataframe", "Identificando valores NaN", "Gráficos", "Modelo de Random Fortest Treinado"])
+
+if selected_page == "Dataframe":
+    st.write('## Dataframe sobre a campanha')
+    df
+    text_content = """
+    ## Variáveis Bancárias
+
+    **age (Idade):** Variável numérica que representa a idade do cliente.
+
+    **job (Emprego):** Variável categórica que descreve o emprego do cliente, com categorias como 'admin.', 'blue-collar', 'entrepreneur', etc.
+
+    **marital (Status Conjugal):** Variável categórica que indica o estado conjugal do cliente, com categorias como 'divorced', 'married', 'single', etc. (Nota: 'divorced' representa divorciado ou viúvo).
+
+    **education (Escolaridade):** Variável categórica que descreve a escolaridade do cliente, com categorias como 'basic.4y', 'basic.6y', 'basic.9y', etc.
+
+    **default (Inadimplente):** Variável categórica que indica se o cliente é inadimplente, com categorias 'no', 'yes' e 'unknown'.
+
+    **housing (Financiamento Imobiliário):** Variável categórica que descreve se o cliente possui financiamento imobiliário, com categorias 'no', 'yes' e 'unknown'.
+
+    **loan (Empréstimo Pessoal):** Variável categórica que descreve se o cliente possui empréstimo pessoal, com categorias 'no', 'yes' e 'unknown'.
+
+
+    ## Variáveis Campanha
+
+    **contact (Canal de Contato):** Variável categórica que indica o canal de contato utilizado, com categorias 'cellular' e 'telephone'.
+
+    **month (Mês de Contato):** Variável categórica que descreve o mês em que o cliente foi contatado, com valores como 'jan', 'feb', 'mar', etc.
+
+    **day_of_week (Dia da Semana):** Variável categórica que indica o dia da semana em que o cliente foi contatado, com valores como 'mon', 'tue', 'wed', etc.
+
+    **duration(s) (Duração em Segundos):** Variável numérica que representa a duração do contato em segundos.
+
+    **campaign (Número de Contatos):** Variável numérica que indica o número de contatos realizados durante esta campanha para este cliente.
+
+    **pdays (Dias desde o Último Contato):** Variável numérica que indica o número de dias desde o último contato da campanha anterior (999 significa que o cliente não foi contatado anteriormente).
+
+    **previous (Contatos Anteriores):** Variável numérica que representa o número de contatos realizados antes desta campanha para este cliente.
+
+    **poutcome (Resultado da Campanha Anterior):** Variável categórica que descreve o resultado da campanha de marketing anterior, com categorias como 'fracasso', 'inexistente' e 'sucesso'.
+
+
+    ## Variáveis Contexto Socioeconômico
+
+    **emp.var.rate (Taxa de Variação de Emprego):** Variável numérica que representa a taxa de variação do emprego.
+
+    **cons.price.idx (Índice de Preços ao Consumidor):** Variável numérica que representa o índice de preços ao consumidor.
+
+    **cons.conf.idx (Índice de Confiança do Consumidor):** Variável numérica que representa o índice de confiança do consumidor.
+
+    **euribor3m (Taxa de Juros a 3 Meses):** Variável numérica que representa a taxa de juros média dos empréstimos interbancários a 3 meses.
+
+    **n°.empregados (Número de Empregados):** Variável numérica que indica o número de empregados.
+
+    ## Target
+
+    **y (O Cliente Aderiu ao Produto?):** Variável binária que indica se o cliente aderiu ao produto, com valores 'yes' (sim) e 'no' (não).
+    """
+
+    # Display the text content using Streamlit
+    st.markdown(text_content)
+
+elif selected_page == 'Identificando valores NaN':
+    search_values = [np.nan, 'unknown', 'nonexistent']
+    for value in search_values:
+        st.subheader(f'Histogram for "{value}"')
+        
+        # Initialize a count for each column
+        counts = []
+        
+        # Iterate through columns
+        for column in df.columns:
+            # Count occurrences of the value in the column
+            count = df[column].apply(lambda x: x == value).sum()
+            counts.append(count)
+        
+        # Create a bar plot for the counts
+        plt.figure(figsize=(8, 4))
+        plt.title(f'Histogram for "{value}"')
+        sns.barplot(x=counts, y=df.columns)
+        plt.xlabel('Count')
+        plt.ylabel('Columns')
+        
+        # Display the plot in Streamlit
+        st.pyplot()
+
+elif selected_page == "Gráficos":
+    chart_container = st.container()
+
+    # Histogram with custom figure size
+    with chart_container:
+        st.write('## Gráfico de barras demonstra x')
+        col1, col2 = st.columns(2)
+        fig_hist = px.histogram(df, x='age', title='Histograma de Idade')
+        col1.plotly_chart(fig_hist)
+
+    # Pie chart with custom figure size
+    with chart_container:
+        st.write('## Gráfico de pizza demonstra y')
+        col3, col4 = st.columns(2)
+        df['marital_counts'] = df['marital'].value_counts()
+        fig_marital = px.pie(df, names='marital', title='Distribuição Marital')
+        col3.plotly_chart(fig_marital)
+
+elif selected_page == "Modelo de Random Fortest Treinado":
+    # Split the data and train the model
+    X = df.drop(columns=['y'])
+    y = df['y']
+    X = pd.get_dummies(X)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
+    rf_model.fit(X_train, y_train)
+
+    # Calculate accuracy
+    accuracy = rf_model.score(X_test, y_test)
+    st.write(f'Accuracy on the test set: {accuracy:.2f}')
+
+    # Plot feature importance
+    feature_importance = rf_model.feature_importances_
+    sorted_idx = feature_importance.argsort()
+
+    fig, ax = plt.subplots(figsize=(15, 9))
+    ax.barh(range(X.shape[1]), feature_importance[sorted_idx])
+    ax.set_yticks(range(X.shape[1]))
+    ax.set_yticklabels(X.columns[sorted_idx])
+    ax.set_xlabel('Feature Importance')
+    ax.set_title('Random Forest Feature Importance')
+
+    # Show the plot using st.pyplot
+    st.pyplot(fig)
+
